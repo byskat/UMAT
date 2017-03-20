6e8bdf2d889ed03e74559a762ccd8a0b8c0e937b
byskat <victor.alarcon.serrano@gmail.com>
2017-03-20 15:46:12 +0100

    CSS to SASS and style optimitzation

--- resources/php/includes/headerViews.php
+++ resources/php/includes/headerViews.php
@@ -8,22 +8,22 @@
 -->
 <div id="headerViews" class="homeLocal row no-gutter">
 	<div class="col-xs-3">
-		<figure>
+		<figure class="stylizedImg">
 			<a href="#"><img class="img-fluid" src="http://placehold.it/350X250"></a>
 		</figure>
 	</div>
 	<div class="col-xs-3">
-		<figure>
+		<figure class="stylizedImg">
 			<a href="#"><img class="img-fluid" src="http://placehold.it/350X250"></a>
 		</figure>
 	</div>
 	<div class="col-xs-3">
-		<figure>
+		<figure class="stylizedImg">
 			<a href="#"><img class="img-fluid" src="http://placehold.it/350X250"></a>
 		</figure>
 	</div>
 	<div class="col-xs-3">
-		<figure>
+		<figure class="stylizedImg">
 			<a href="#"><img class="img-fluid" src="http://placehold.it/350X250"></a>
 		</figure>
 	</div>
